a431c9b2a9d9f8e551b9e8fcae9beeb49e40e21f
ramitsawhney27 <24762556+ramitsawhney27@users.noreply.github.com>
2018-05-19 19:44:46 +0530

    Optimized Stemming (#20)
    * Optimized Stemming
    
    Generator expressions with joins are much faster than conversion to strings and appending to stemmed tokens to lists and overheads of string conversion. Also did some minor cleanup.
    
    * FIxed minor error.
    
    * Fixed

--- main_server.py
+++ main_server.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import request, json
 from github_functions import label_opened_issue
-from stemming.porter2 import stem
+from stemming.porter2 import stemmer
 from nltk.tokenize import word_tokenize
 
 app = Flask(__name__)
@@ -11,30 +11,28 @@ app = Flask(__name__)
 def home():
     return 'Response to test hosting.'
 
-#this function will recieve all the github events
+#This function will recieve all the github events
 @app.route('/web_hook', methods=['POST'])
 def github_hook_receiver_function():
     if request.headers['Content-Type'] == 'application/json':
         data = request.json
         try:
-            # This try catch is checking if action key is present in event data passed by hook
+            #This try-catch is checking if action key is present in event data passed by hook
             action = data['action']
             if action == 'opened':
                 # If it's an issue opened event
                 label_opened_issue(data)
-            # No other events are being handeled currently
+            #No other events are being handled currently
         except KeyError:
-            #currently the bot isn't handeling any other cases
+            #Currently the bot isn't handling any other cases
             pass
         return json.dumps(request.json)
 
 
 def get_stems(sentence):
-    result = list()
-    tokens = word_tokenize(sentence)
-    for word in tokens:
-        result.append(stem(word))
-    return result
+    #Generator expressions with joins are much faster than conversion to strings and appending to stemmed tokens to lists and overheads of string conversion.
+    stemmed_sentence = ' '.join(stemmer.stem(token) for token in word_tokenize(sentence))
+    return stemmed_sentence
 
 if __name__ == '__main__':
     app.run()
