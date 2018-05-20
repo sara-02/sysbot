7e90cc9161c4d98979e569a25ba4e7fec4d850d1
ramitsawhney27 <24762556+ramitsawhney27@users.noreply.github.com>
2018-05-20 16:35:26 +0530

    Added lemmatizing. (#23)
    Added lemmatizing to allow both functionalities open as Sysbot scales.

--- main_server.py
+++ main_server.py
@@ -3,6 +3,7 @@ from flask import request, json
 from github_functions import label_opened_issue
 from stemming.porter2 import stemmer
 from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
 
 app = Flask(__name__)
 
@@ -32,6 +33,11 @@ def get_stems(sentence):
     #Generator expressions with joins are much faster than conversion to strings and appending to stemmed tokens to lists and overheads of string conversion.
     stemmed_sentence = ' '.join(stemmer.stem(token) for token in word_tokenize(sentence))
     return stemmed_sentence
+
+def lemmatize_sent(sentence):
+    #Generator expressions with joins are much faster than conversion to strings and appending to stemmed tokens to lists and overheads of string conversion.
+    lemmatized_sentence = ' '.join(WordNetLemmatizer().lemmatize(token) for token in word_tokenize(sentence))
+    return lemmatized_sentence
 
 if __name__ == '__main__':
     app.run()
