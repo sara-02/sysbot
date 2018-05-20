72ca8835a79147c8be811c13c3fa76e30b59dd43
sammy1997 <sombuddha2016@gmail.com>
2018-05-20 18:58:04 +0530

    Added message file and initial message. Fixes issue #15

---
+++ messages.py
@@ -0,0 +1,15 @@
+
+MESSAGE = {
+    'first_timer_message': 'Hello!\nWelcome to the Systers Open Source community!\nHow to get started:'+\
+        '\n• Join #intro and introduce yourself! :slightly_smiling_face:\n• Join the #code-of-conduct and read the community rules.'+\
+        '\n         - Update your profile in accordance with the community rules.'+\
+        '\n• Find and join the other channels relevant to you (i.e. different projects).'+\
+        '\nIf you have any #questions, post them in the #questions channel! :slightly_smiling_face:\nTag @may if no one answers immediately.'+\
+        '\nTo get more information or ask questions about the larger community, post in #systers.'+\
+        '\nTo track GitHub activity of ALL Systers\' repos, join the #github-activity channel.'+\
+        '\nTo keep up with the Systers-Dev email list, join #mailing-list.'+\
+        '\nTo give any thanks or recognition to other members, shoutout in the #celebrate channel.'
+        '\n\nHow to operate Sysbot?'+\
+        '\nType /sysbot_help'
+        #More will be added as we go
+}
