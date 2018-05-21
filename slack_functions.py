fa744d704b7c78737ce62bcf2673e796a1a0b34f
sammy1997 <sombuddha2016@gmail.com>
2018-05-21 22:05:58 +0530

    Added function to recognize if a comment is made by a mentor or not. Fixes issue #9

--- slack_functions.py
+++ slack_functions.py
@@ -1,16 +1,19 @@
+# -*- coding: utf-8 -*-
+
 import requests
 from flask import request, json, jsonify
-from auth_credentials import  BOT_ACCESS_TOKEN
-from request_urls import dm_channel_open_url, dm_chat_post_message_url
+from auth_credentials import  BOT_ACCESS_TOKEN, maintainer_usergroup_id, legacy_token
+from request_urls import dm_channel_open_url, dm_chat_post_message_url, get_maintainer_list
 from messages import MESSAGE
 
+headers = {'Content-type': 'application/json', 'Authorization': 'Bearer {}'.format(BOT_ACCESS_TOKEN)}
+headers_legacy_urlencoded = {'Content-type': 'application/x-www-form-urlencoded', 'Authorization': 'Bearer {}'.format(legacy_token)}
 
 def dm_new_users(data):
     #Get user id of the user who joined
     uid = data.get('event',{}).get('user',None)
     if uid != None:
         body = {'user': uid}
-        headers = {'Content-type': 'application/json', 'Authorization': 'Bearer {}'.format(BOT_ACCESS_TOKEN)}
         #Open a DM channel to the user. Request goes to im.open
         r = requests.post(dm_channel_open_url, data=json.dumps(body), headers=headers)
         response = r.json()
@@ -24,3 +27,20 @@ def dm_new_users(data):
                 print("Response: %s" % json.dumps(r.json()))
                 return {'message':'Success','status': 200}
     return {'message':'Data format wrong', 'status':400}
+
+
+def is_maintainer_comment(commenter_id):
+    #Using the id of usergroup maintainers
+    body = {'usergroup': maintainer_usergroup_id, 'include_disabled': True}
+    #Get list of maintainers. For more info :  usergroups.users.list in Slack API
+    r = requests.post(get_maintainer_list, data= body, headers=headers_legacy_urlencoded)
+    response = r.json()
+    if response.get('ok',False):
+        #Extract the maintainers
+        maintainers = response.get('users',[])
+        if commenter_id in maintainers:
+            return {'status': 200, 'is_maintainer': True}
+        else:
+            return {'status': 200, 'is_maintainer': True}
+    else:
+        return {'status': 400, 'message': 'Wrong parameters'}
