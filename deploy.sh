2e209c323c91763b084f6b9f5100b9c0a194aff8
Sombuddha Chakravarty <sombuddha2016@gmail.com>
2018-07-07 19:54:47 +0530

    Auto-deploy and selective testing script. Fixes #77 (#96)

---
+++ deploy.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+# Travis build triggered on a forked repository
+if [ "$TRAVIS_REPO_SLUG" != "systers/sysbot" ]; then
+    echo "Skip publishing from forked repo."
+    exit 0
+fi
+
+# Travis build triggered by a PR
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+    echo "Skip publishing, just a PR."
+    exit 0
+fi
+
+# Checking branch
+if [ "$TRAVIS_BRANCH" == "develop" ]; then
+    FLASK_ENVIRONMENT_CONFIG="dev"
+    SERVER="Staging"
+elif [ "$TRAVIS_BRANCH" == "production" ]; then
+    FLASK_ENVIRONMENT_CONFIG="prod"
+    SERVER="Production"
+else
+    echo "Skip publishing, we don't publish for '$TRAVIS_BRANCH' branch"
+    exit 0
+fi
+
+# Get the latest code
+cd $HOME
+git clone --branch=$TRAVIS_BRANCH https://github.com/systers/sysbot.git
+cd sysbot
+
+# Create AWS Elastic Beanstalk profile
+mkdir ~/.aws
+echo "[profile eb-cli]" > ~/.aws/config
+echo "aws_access_key_id = $AWS_ACCESS_ID" >> ~/.aws/config
+echo "aws_secret_access_key = $AWS_SECRET_KEY" >> ~/.aws/config
+
+# Add environment variables
+eb setenv FLASK_ENVIRONMENT_CONFIG=$FLASK_ENVIRONMENT_CONFIG
+eb setenv USERNAME=$USERNAME
+eb setenv PASSWORD=$PASSWORD
+eb setenv path_secret=$path_secret
+eb setenv api_key=$api_key
+eb setenv BOT_ACCESS_TOKEN=$BOT_ACCESS_TOKEN
+eb setenv legacy_token=$legacy_token
+
+# Publishing
+echo "Publishing to '$SERVER' server"
+eb deploy
+if [ $? -eq 0 ]; then
+    echo "Publishing successful."
+else
+    echo "Publishing failed."
+    exit 2
+fi
